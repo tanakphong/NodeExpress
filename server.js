f0f7de62d6bd0befdd50830d0d228a16714e1d87
tanakphong <t.phongsing@gmail.com>
2018-03-12 15:44:21 +0700

    add Item model and getAllItem

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const store = require('./store')
 const app = express()
+
+store.init()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/item/', (req, res) => {
+  res.send({ items: store.getAllItems() })
+})
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
